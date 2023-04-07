39b459ad9209d507bcd3cef44de41190ff648dbd
sakioo <53650208+thomaue@users.noreply.github.com>
2023-04-07 02:19:28 +0200

    finished pointer course

--- my-app/src/screens/Pointeur.tsx
+++ my-app/src/screens/Pointeur.tsx
@@ -1,6 +1,10 @@
 import img1 from '../Images/mémoire.jpg'
 import img2 from '../Images/code1.jpg'
 import img3 from '../Images/test1.jpg'
+import img4 from '../Images/mémoire2.jpg'
+import img5 from '../Images/difference.jpg'
+import img6 from '../Images/magie.jpg'
+import img7 from '../Images/mémoire3.jpg'
 
 
 export const Pointeur = () => {
@@ -55,7 +59,7 @@ export const Pointeur = () => {
             </h4>
 
             <br />
-            <p> pour mieux comprendre étudions de près cet exemple :</p>
+            <p> pour mieux comprendre son utilité étudions de près cet exemple :</p>
 
             <img src={img2} alt=""/>
 
@@ -77,7 +81,105 @@ export const Pointeur = () => {
                 <br />
             </p>
 
-            <h2>mais alors que s'est-t-il passé ?</h2>
+            <h2>Que s'est-t-il passé ?</h2>
+
+            <img src={img4} alt=""/>
+
+            <p>
+                nous avons grâce à la fonction crée des copies de
+                <em> nb1 </em>
+                et
+                <em> nb2 </em>
+                . 
+                <br />
+                <br />
+                Ainsi dans notre exemple, ce sont 
+                <em> nombre1 </em>
+                et 
+                <em> nombre2 </em>
+                qui ont permutés, et non pas
+                <em> nb1 </em>
+                et
+                <em> nb2 </em>
+                comme souhaité.
+            </p>
+
+            <h2>Comment créer un pointeur ?</h2>
+
+            <p>
+                pour déclarer ou initialiser un pointeur
+                 on utilise la syntaxe
+                suivante :
+                <br />
+                <br />
+                <mark>
+                <em>type </em>
+                *nom_pointeur = &nom_variable 
+                </mark>
+                <br />
+                <br />
+                <h4>par exemple :</h4>
+                <em>int </em>
+                *nombre1 = &nb1
+                <br />
+                <br />
+                <br />
+
+                enfin pour utiliser un pointeur on utilisera cette
+                syntaxe :
+                <br />
+                <br />
+                <em>nom_pointeur </em>
+                : recupère 
+                <mark>l'adresse de la variable </mark>
+                pointée
+                <br />
+                <em>*nom_pointeur </em>
+                : recupère la 
+                <mark> valeur de la variable </mark> 
+                pointée
+                <br />
+                <em>&nom_pointeur </em>
+                : recupère 
+                <mark> l'adresse du pointeur </mark>
+            </p>
+
+            <h2>Résoudre le problème : </h2>
+
+            <p>
+                on déclare donc les pointeurs
+                <em> nombre1 </em>
+                et 
+                <em> nombre2 </em>
+                qui pointe respectivement sur 
+                <em> nb1 </em>
+                et
+                <em> nb 2 </em>
+            </p>
+            <img src={img5} alt=""/>
+
+            <h4>  Magie *_*</h4>
+
+            <img src={img6} alt=""/>
+
+            <h2>en conclusion :</h2>
+
+            <p>voici ce que nous avons fait :</p>
+            <br />
+            <img src={img7} alt=""/>
+            <br />
+            en utilisant les *pointeurs nous avons pu acceder aux 
+            valeurs contenue par 
+            <em> nb1 </em>
+                et
+            <em> nb 2 </em>
+
+
+
+            
+
+
+
             
 
 
